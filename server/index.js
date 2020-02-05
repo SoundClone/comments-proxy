4dde3919062a7e9eb640d1d75cab7ee62049aa1c
sean w <52753311+vtsu3@users.noreply.github.com>
2020-02-05 14:32:47 -0800

    basic proxy setup

--- server/index.js
+++ server/index.js
@@ -1,1 +1,27 @@
+// Setup
 const express = require('express');
+const path = require('path');
+const morgan = require('morgan');
+const axios = require('axios');
+const cors = require('cors');
+
+const app = express();
+
+// Middleware
+app.use(express.static(path.join(__dirname, '../public')));
+app.use(cors());
+app.use(morgan('dev'));
+app.use(express.json());
+
+// Routes
+app.get('/api/comments', (req, res) => {
+  axios.get('http://localhost:3001/api/comments')
+    .then((res1) => {
+      console.log('HELLO WORLD')
+      res.send(res1.data);
+    });
+});
+
+// Initialise
+const PORT = 3000;
+app.listen(PORT, () => { console.log(`listening on http://localhost:${PORT}`) });
